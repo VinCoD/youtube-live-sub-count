e8f60fb925f4f86b15aab0cc740839f2ef74dcc7
Odipo Otieno <5895.2020@students.ku.ac.ke>
2024-08-17 22:09:24 +0300

    Added a subscribe button

--- src/LiveSubCount.js
+++ src/LiveSubCount.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getChannelDetails } from './api/youtube';
+import { CheckCircleIcon } from '@heroicons/react/20/solid';
 
 const LiveSubCount = () => {
   const [channelDetails, setChannelDetails] = useState({
@@ -26,7 +27,7 @@ const LiveSubCount = () => {
         <img 
           src={channelDetails.profilePicture} 
           alt="Channel Thumbnail" 
-          className="w-24 h-24 rounded-full border-4 border-white" 
+          className="w-24 h-24 rounded-full" 
         />
         <h1 className="text-4xl font-semibold">{channelDetails.channelName}</h1>
         <div className="text-7xl font-bold">
@@ -36,6 +37,16 @@ const LiveSubCount = () => {
           }
         </div>
         <p className="text-xl">Subscribers</p>
+        
+        <a 
+          href="https://www.youtube.com/@kwargdevs?sub_confirmation=1" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="inline-flex items-center gap-x-2 rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+        >
+          Subscribe
+          <CheckCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+        </a>
       </div>
     </div>
   );
