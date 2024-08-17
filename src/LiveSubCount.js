347c0605cdc2e0340859a789d84807df5e32da6c
Odipo Otieno <5895.2020@students.ku.ac.ke>
2024-08-17 21:35:17 +0300

    Added some styling to the project

--- src/LiveSubCount.js
+++ src/LiveSubCount.js
@@ -1,25 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { getSubscriberCount } from './api/youtube';
+import { getChannelDetails } from './api/youtube';
 
 const LiveSubCount = () => {
-  const [subCount, setSubCount] = useState(null);
+  const [channelDetails, setChannelDetails] = useState({
+    subscriberCount: null,
+    channelName: '',
+    profilePicture: ''
+  });
 
-  const fetchSubCount = async () => {
-    const count = await getSubscriberCount();
-    setSubCount(count);
+  const fetchChannelDetails = async () => {
+    const details = await getChannelDetails();
+    setChannelDetails(details);
   };
 
   useEffect(() => {
-    fetchSubCount();
-    const intervalId = setInterval(fetchSubCount, 3000); // Fetch every 5 seconds
+    fetchChannelDetails();
+    const intervalId = setInterval(fetchChannelDetails, 3000); // Fetch every 3 seconds
 
     return () => clearInterval(intervalId); // Clean up on unmount
   }, []);
 
   return (
-    <div>
-      <h1>Live Subscriber Count</h1>
-      {subCount !== null ? <h2>{subCount}</h2> : <p>Loading...</p>}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      <div className="flex flex-col items-center space-y-4">
+        <img 
+          src={channelDetails.profilePicture} 
+          alt="Channel Thumbnail" 
+          className="w-24 h-24 rounded-full" 
+        />
+        <h1 className="text-4xl font-semibold">{channelDetails.channelName}</h1>
+        <div className="text-7xl font-bold">
+          {channelDetails.subscriberCount !== null ? 
+            parseInt(channelDetails.subscriberCount).toLocaleString() : 
+            'Loading...'
+          }
+        </div>
+        <p className="text-xl">Subscribers</p>
+      </div>
     </div>
   );
 };
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_API
+const API_KEY = process.env.REACT_APP_YOUTUBE_API;
 const CHANNEL_ID = 'UCdMBvHugZtN_FZ6oh8L5BHA';
 
-export const getSubscriberCount = async () => {
+export const getChannelDetails = async () => {
   const response = await axios.get(
-    `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY}`
+    `https://www.googleapis.com/youtube/v3/channels?part=statistics,snippet&id=${CHANNEL_ID}&key=${API_KEY}`
   );
-  return response.data.items[0].statistics.subscriberCount;
+  
+  const data = response.data.items[0];
+  
+  return {
+    subscriberCount: data.statistics.subscriberCount,
+    channelName: data.snippet.title,
+    profilePicture: data.snippet.thumbnails.default.url
+  };
 };
