d64d61159064e8ff6f07b147c88da50dfe1d9787
Odipo Otieno <5895.2020@students.ku.ac.ke>
2024-08-18 16:37:48 +0300

    Update LiveSubCount.js

--- src/LiveSubCount.js
+++ src/LiveSubCount.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getChannelDetails } from './api/youtube';
-import { CheckCircleIcon } from '@heroicons/react/20/solid';
+import { ArrowUpRightIcon } from '@heroicons/react/20/solid';
 
 const LiveSubCount = () => {
   const [channelDetails, setChannelDetails] = useState({
