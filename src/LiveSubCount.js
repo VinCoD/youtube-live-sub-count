eb34dd895a25c2aa258fa6301ee90f5cb5e34cc4
Odipo Otieno <5895.2020@students.ku.ac.ke>
2024-08-18 16:38:25 +0300

    Update LiveSubCount.js

--- src/LiveSubCount.js
+++ src/LiveSubCount.js
@@ -45,7 +45,7 @@ const LiveSubCount = () => {
           className="inline-flex items-center gap-x-2 rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
         >
           Subscribe
-          <CheckCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+          <ArrowUpRightIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
         </a>
       </div>
     </div>
